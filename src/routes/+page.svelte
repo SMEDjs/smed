9e1e68992323c63e7e37e36d2bfd27d217c6bb95
iSwed DSG <chachefavennec@gmail.com>
2023-03-22 21:34:06 +0100

    setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	console.log('test');
+	console.log('flop');
+</script>
+
+<h1>SMED</h1>
+<p>A motivated AND HUGE HUGE HUGE HUGE developer</p>
