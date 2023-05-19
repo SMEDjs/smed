bf28485e59530948203f33cdedaa143755ce428c
iSwed DSG <chachefavennec@gmail.com>
2023-05-19 17:24:17 +0200

    add gif support

--- src/routes/api/cdn/[imageId]/+server.ts
+++ src/routes/api/cdn/[imageId]/+server.ts
@@ -4,6 +4,7 @@ import { error } from '@sveltejs/kit';
 import { prisma } from '$lib/server/prisma';
 import type { RequestHandler } from './$types';
 import Jimp from 'jimp';
+import gifwrap from "gifwrap"
 
 export const GET = (async ({ params }) => {
 	try {
@@ -14,21 +15,25 @@ export const GET = (async ({ params }) => {
 				id
 			}
 		});
+		const response = await fetch(image.url);
+		if(response.status === 404) throw error(404) 
+
+		let buffer = await response.arrayBuffer();
 		const img = await Jimp.read(image.url);
-		const icon = await Jimp.read(process.env.ICON_URL ?? "");
+		if(img.getMIME() !== Jimp.MIME_GIF) {
+			const icon = await Jimp.read(process.env.ICON_URL ?? "");
+			const iconSize = Math.min(img.getWidth(), img.getHeight()) * 0.4;
+			icon.contain(iconSize, iconSize);
+			img.composite(icon, 0, img.getHeight() - icon.getHeight()) 
+			buffer = await img.getBufferAsync(img.getMIME());
+		}
 
-		const iconSize = Math.min(img.getWidth(), img.getHeight()) * 0.4;
-		icon.contain(iconSize, iconSize);
-		img.composite(icon, 0, img.getHeight() - icon.getHeight()) 
-
-		const imageBuffer = await img.getBufferAsync(Jimp.MIME_PNG);
 		const headers = new Headers({
-			'Content-Type': 'image/png',
-			'Content-Length': imageBuffer.length.toString()
+			'Content-Type': img.getMIME(),
+			'Content-Length': buffer.byteLength.toString()
 		});
-
-		const response = new Response(imageBuffer, { headers });
-		return Promise.resolve(response);
+		const result = new Response(buffer, { headers });
+		return Promise.resolve(result);
 	} catch (err) {
 		console.log(err);
 		throw error(400, 'Something wrong happend');
